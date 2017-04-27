b397e198a0869d1122e978901eada093626892a5
Rya Meyvriska <rya.meyvriska@gmail.com>
2017-04-27 23:35:06 +0700

    Tambah main presenter

---
+++ Android/app/src/main/java/id/jeruk/ok_safe/presenter/MainPresenter.java
@@ -0,0 +1,15 @@
+package id.jeruk.ok_safe.presenter;
+
+/**
+ * Created by RyMey on 4/27/17.
+ */
+
+public class MainPresenter extends BasePresenter<MainPresenter.View> {
+    public MainPresenter(View view) {
+        super(view);
+    }
+
+    public interface View extends BasePresenter.View {
+
+    }
+}
