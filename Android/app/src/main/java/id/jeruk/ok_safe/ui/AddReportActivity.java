356db14726e2610e76f8a93936f67f17717c77b0
Rya Meyvriska <rya.meyvriska@gmail.com>
2017-04-28 11:00:35 +0700

    Handle pick location

--- Android/app/src/main/java/id/jeruk/ok_safe/ui/AddReportActivity.java
+++ Android/app/src/main/java/id/jeruk/ok_safe/ui/AddReportActivity.java
@@ -1,17 +1,28 @@
 package id.jeruk.ok_safe.ui;
 
-import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.widget.TextView;
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.ui.PlacePicker;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import id.jeruk.ok_safe.R;
 import id.jeruk.ok_safe.util.Util;
 
 public class AddReportActivity extends AppCompatActivity {
+    private static final int PLACE_PICKER_REQUEST = 1;
+
+    @BindView(R.id.address) TextView tvAddress;
+
     private String imgUrl[];
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,8 +30,18 @@ public class AddReportActivity extends AppCompatActivity {
         ButterKnife.bind(this);
     }
 
+    @OnClick(R.id.pick_location)
+    public void pickLocation() {
+        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+        try {
+            startActivityForResult(builder.build(this), PLACE_PICKER_REQUEST);
+        } catch (GooglePlayServicesRepairableException | GooglePlayServicesNotAvailableException e) {
+            e.printStackTrace();
+        }
+    }
+
     @OnClick(R.id.iv_back)
-    public void back(){
+    public void back() {
         onBackPressed();
     }
 
@@ -29,12 +50,10 @@ public class AddReportActivity extends AppCompatActivity {
         String[] inputPhotoOptions = {"From Camera", "From Gallery"};
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setItems(inputPhotoOptions, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
-                if (which == 0) Util.openCamera(AddReportActivity.this);
-                else if (which == 1) Util.openGallery(AddReportActivity.this);
-                dialog.dismiss();
-            }
+        builder.setItems(inputPhotoOptions, (dialog, which) -> {
+            if (which == 0) Util.openCamera(AddReportActivity.this);
+            else if (which == 1) Util.openGallery(AddReportActivity.this);
+            dialog.dismiss();
         }).show();
     }
 
@@ -43,12 +62,10 @@ public class AddReportActivity extends AppCompatActivity {
         String[] inputPhotoOptions = {"From Camera", "From Gallery"};
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setItems(inputPhotoOptions, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
-                if (which == 0) Util.openCamera(AddReportActivity.this);
-                else if (which == 1) Util.openGallery(AddReportActivity.this);
-                dialog.dismiss();
-            }
+        builder.setItems(inputPhotoOptions, (dialog, which) -> {
+            if (which == 0) Util.openCamera(AddReportActivity.this);
+            else if (which == 1) Util.openGallery(AddReportActivity.this);
+            dialog.dismiss();
         }).show();
     }
 
@@ -57,12 +74,18 @@ public class AddReportActivity extends AppCompatActivity {
         String[] inputPhotoOptions = {"From Camera", "From Gallery"};
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setItems(inputPhotoOptions, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
-                if (which == 0) Util.openCamera(AddReportActivity.this);
-                else if (which == 1) Util.openGallery(AddReportActivity.this);
-                dialog.dismiss();
-            }
+        builder.setItems(inputPhotoOptions, (dialog, which) -> {
+            if (which == 0) Util.openCamera(AddReportActivity.this);
+            else if (which == 1) Util.openGallery(AddReportActivity.this);
+            dialog.dismiss();
         }).show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == PLACE_PICKER_REQUEST) {
+            tvAddress.setText(PlacePicker.getPlace(this, data).getAddress());
+        }
     }
 }
