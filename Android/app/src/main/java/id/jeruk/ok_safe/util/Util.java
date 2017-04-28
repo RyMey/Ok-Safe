4a8f1664f85cf952aba98abc52d4d7a1ee0d87a0
Rya Meyvriska <rya.meyvriska@gmail.com>
2017-04-28 10:19:09 +0700

    Add img load in util

--- Android/app/src/main/java/id/jeruk/ok_safe/util/Util.java
+++ Android/app/src/main/java/id/jeruk/ok_safe/util/Util.java
@@ -3,9 +3,13 @@ package id.jeruk.ok_safe.util;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.provider.MediaStore;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+
+import java.io.InputStream;
+import java.net.URL;
 
 /**
  * Created by RyMey on 3/30/17.
@@ -34,6 +38,15 @@ public class Util {
         intent.setType("image/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
         context.startActivity(intent);
+    }
+
+    public static Drawable LoadImageFromWebOperations(String url) {
+        try {
+            InputStream is = (InputStream) new URL(url).getContent();
+            return Drawable.createFromStream(is, "src name");
+        } catch (Exception e) {
+            return null;
+        }
     }
 
 }
