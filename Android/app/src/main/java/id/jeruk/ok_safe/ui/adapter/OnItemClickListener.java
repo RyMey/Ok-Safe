239d90c4d94f2de81b64ddb40b7c157cbce582d4
Rya Meyvriska <rya.meyvriska@gmail.com>
2017-04-28 14:47:30 +0700

    Handle showing report cards

---
+++ Android/app/src/main/java/id/jeruk/ok_safe/ui/adapter/OnItemClickListener.java
@@ -0,0 +1,7 @@
+package id.jeruk.ok_safe.ui.adapter;
+
+import android.view.View;
+
+public interface OnItemClickListener {
+    void onItemClick(View view, int position);
+}
---
+++ Android/app/src/main/java/id/jeruk/ok_safe/ui/adapter/OnLongItemClickListener.java
@@ -0,0 +1,7 @@
+package id.jeruk.ok_safe.ui.adapter;
+
+import android.view.View;
+
+public interface OnLongItemClickListener {
+    void onLongItemClick(View view, int position);
+}
