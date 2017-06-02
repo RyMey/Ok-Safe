755ce48ce5ee7747f8bc09705dbb09a58ff7a217
Rya Meyvriska <rya.meyvriska@gmail.com>
2017-06-02 22:59:15 +0700

    Handle showing peta sebaran

--- Android/app/src/main/java/id/jeruk/ok_safe/ui/MapActivity.java
+++ Android/app/src/main/java/id/jeruk/ok_safe/ui/MapActivity.java
@@ -1,23 +1,154 @@
 package id.jeruk.ok_safe.ui;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import id.jeruk.ok_safe.R;
+import id.jeruk.ok_safe.util.Util;
 
-public class MapActivity extends AppCompatActivity {
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    private GoogleMap mMap;
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
         ButterKnife.bind(this);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        if (!requestPermission()) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+    }
+
+    private boolean requestPermission() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+            return true;
+        }
+
+        return false;
     }
 
     @OnClick(R.id.iv_back)
-    public void back(){
+    public void back() {
         onBackPressed();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        if (!requestPermission()) {
+            mMap.setMyLocationEnabled(true);
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (requestPermission()) {
+            return;
+        }
+        getMyLocation();
+    }
+
+    private void getMyLocation() {
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (mLastLocation != null) {
+            LatLng latLng = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
+            mMap.addMarker(new MarkerOptions().position(latLng).title("Laporan 1"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 12.0f));
+            generateLocations();
+        }
+    }
+
+    private void generateLocations() {
+        for (int i = 0; i < 33; i++) {
+            double lat = Util.randInt(0, 3) == 2 ? mLastLocation.getLatitude() + (Util.randInt(1, 10) / 157f)
+                    : mLastLocation.getLatitude() - (Util.randInt(1, 10) / 157f);
+            double lon = Util.randInt(0, 3) == 2 ? mLastLocation.getLongitude() + (Util.randInt(1, 10) / 125f)
+                    : mLastLocation.getLongitude() - (Util.randInt(1, 10) / 175f);
+            LatLng latLng = new LatLng(lat, lon);
+            mMap.addMarker(new MarkerOptions().position(latLng).title("Laporan " + (i + 2)));
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    protected void onStart() {
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.connect();
+        }
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.disconnect();
+        }
+        super.onStop();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (mGoogleApiClient == null) {
+                mGoogleApiClient = new GoogleApiClient.Builder(this)
+                        .addConnectionCallbacks(this)
+                        .addOnConnectionFailedListener(this)
+                        .addApi(LocationServices.API)
+                        .build();
+
+                mGoogleApiClient.connect();
+            } else {
+                getMyLocation();
+            }
+            mMap.setMyLocationEnabled(true);
+
+        } else {
+            Toast.makeText(this, "Perbolehkan permission untuk menggunakan fitur!", Toast.LENGTH_SHORT).show();
+            requestPermission();
+        }
     }
 }
